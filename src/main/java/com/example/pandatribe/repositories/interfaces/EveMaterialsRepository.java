680ee1e42c3f325ee5f207ed5d78af36fe0c47f3
Vladislav <vladski.angelov@gmail.com>
2024-03-27 01:40:57 +0200

    refactor to pg

--- src/main/java/com/example/pandatribe/repositories/interfaces/EveMaterialsRepository.java
+++ src/main/java/com/example/pandatribe/repositories/interfaces/EveMaterialsRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface EveMaterialsRepository extends JpaRepository<Material, MaterialBlueprintKey> {
-    @Query(value = "SELECT * FROM industryActivityMaterials m " +
-            "WHERE m.typeID = ?1 AND (m.activityID = 1 or m.activityID = 11)", nativeQuery = true)
+    @Query(value = "SELECT * FROM evesde.\"industryActivityMaterials\" m " +
+            "WHERE m.\"typeID\" = ?1 AND (m.\"activityID\" = 1 or m.\"activityID\" = 11)", nativeQuery = true)
     List<Material> findMaterialsByActivity(Integer blueprintId);
 }
