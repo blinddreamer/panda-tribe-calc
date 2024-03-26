8d15ee172dd1213e82b810b43dc3ba52a3edd97d
Vladislav <vladski.angelov@gmail.com>
2024-03-26 22:22:26 +0200

    refactor cors

--- src/main/java/com/example/pandatribe/controllers/EveBlueprintController.java
+++ src/main/java/com/example/pandatribe/controllers/EveBlueprintController.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.Objects;
 
 @RestController
-@CrossOrigin(maxAge = 3600)
 @AllArgsConstructor
 @RequestMapping("/api/v1/")
 public class EveBlueprintController {
