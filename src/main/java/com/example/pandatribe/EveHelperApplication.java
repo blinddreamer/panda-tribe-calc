d464372e16e2a8b88b2fafa90558ef622d0a6358
Vladislav <vladski.angelov@gmail.com>
2024-04-18 23:06:49 +0300

    added configuration

--- src/main/java/com/example/pandatribe/EveHelperApplication.java
+++ src/main/java/com/example/pandatribe/EveHelperApplication.java
@@ -3,10 +3,12 @@ package com.example.pandatribe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories
 @EnableTransactionManagement
 @EnableCaching
 @EnableScheduling
